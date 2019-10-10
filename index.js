a0658bda94a746a7682abcb958f8e5a61b535ef7
Logan Glasco <lglasco81@gmail.com>
2019-10-10 18:47:32 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,6 +0,0 @@
-conosle.error(Halp!)
-console.warn(Please Stop!)
-console.log (Take a break)
-console.error(Halp!) , console.warn(Please stop) , console.log (Take a break)
-console.warn("Please stop!")
-console.log("I am a logger")
